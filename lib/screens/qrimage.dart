d5eb78a09e330e7c9f506541b7254c6c6a8aad45
Aswinkora <aswinaloor123@gmail.com>
2024-09-04 11:46:50 +0530

    update

--- lib/screens/qrimage.dart
+++ lib/screens/qrimage.dart
@@ -38,20 +38,71 @@ class QRImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter + QR code'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: QrImageView(
-            data: controller.text,
-            size: 280,
-            // You can include embeddedImageStyle Property if you 
-            //wanna embed an image from your Asset folder
-            embeddedImageStyle: QrEmbeddedImageStyle(
-              size: const Size(
-                100,
-                100,
+      appBar: AppBar(
+        title: const Text('Flutter + QR code'),
+        centerTitle: true,
+      ),
+      body: Row(
+        children: [
+          Flexible(
+              flex: 2,
+              child: Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'COMPLAINT',
+                          style: TextStyle(color: primary, fontSize: 40),
+                        ),
+                        Text(
+                          'BOX',
+                          style: TextStyle(color: second, fontSize: 40),
+                        ),
+                      ],
+                    ),
+                    Text(
+                        'We value your feedback! if you have any concerns,suggestionsor issues,please let us know.'),
+                    Text(
+                        'Your input help us improve our service and address any problem you may be experiencing')
+                  ],
+                ),
+              )),
+          Flexible(
+            flex: 2,
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2,
+              color: primary,
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    // QR Code generation
+                    QrImageView(
+                      backgroundColor: second,
+                      // data: controller.text,
+                      data: controller.text,
+                      size: 280.0,
+                      version: QrVersions.auto,
+                      gapless: false,
+                      errorCorrectionLevel:
+                          QrErrorCorrectLevel.H, // High error correction level
+                    ),
+                    // Circular Avatar at the center of the QR Code
+                    Positioned(
+                      child: CircleAvatar(
+                        backgroundColor:
+                            const Color.fromARGB(255, 223, 200, 200),
+                        backgroundImage:
+                            AssetImage('image/docmeds.png'), // Your image here
+                        radius:
+                            50.0, // Smaller radius to not cover much of the QR code
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
