8e15bcac368b669bec72d81256d65a9515c87ec1
Aswinkora <aswinaloor123@gmail.com>
2024-09-06 16:23:28 +0530

    update

--- lib/screens/qrimage.dart
+++ lib/screens/qrimage.dart
@@ -39,73 +39,205 @@ class QRImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter + QR code'),
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'SERVICE',
+              style: TextStyle(
+                  color: primary, fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            // SizedBox(
+            //   width: 10,
+            // ),
+            Text(
+              'DESK',
+              style: TextStyle(
+                  color: second, fontWeight: FontWeight.bold, fontSize: 25),
+            )
+          ],
+        ),
         centerTitle: true,
       ),
-      body: Row(
+      body: Column(
         children: [
-          Flexible(
-              flex: 2,
-              child: Container(
-                width: MediaQuery.of(context).size.width / 2,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'COMPLAINT',
-                          style: TextStyle(color: primary, fontSize: 40),
-                        ),
-                        Text(
-                          'BOX',
-                          style: TextStyle(color: second, fontSize: 40),
-                        ),
-                      ],
-                    ),
-                    Text(
-                        'We value your feedback! if you have any concerns,suggestionsor issues,please let us know.'),
-                    Text(
-                        'Your input help us improve our service and address any problem you may be experiencing')
-                  ],
+          Container(
+            width: MediaQuery.of(context).size.width,
+            // color: primary,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                // QR Code generation
+                QrImageView(
+                  // backgroundColor: second,
+                  // data: controller.text,
+                  data: controller.text,
+                  size: 280.0,
+                  version: QrVersions.auto,
+                  gapless: false,
+                  errorCorrectionLevel:
+                      QrErrorCorrectLevel.H, // High error correction level
                 ),
-              )),
-          Flexible(
-            flex: 2,
-            child: Container(
-              width: MediaQuery.of(context).size.width / 2,
-              color: primary,
-              child: Center(
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    // QR Code generation
-                    QrImageView(
-                      backgroundColor: second,
-                      // data: controller.text,
-                      data: controller.text,
-                      size: 280.0,
-                      version: QrVersions.auto,
-                      gapless: false,
-                      errorCorrectionLevel:
-                          QrErrorCorrectLevel.H, // High error correction level
-                    ),
-                    // Circular Avatar at the center of the QR Code
-                    Positioned(
-                      child: CircleAvatar(
-                        backgroundColor:
-                            const Color.fromARGB(255, 223, 200, 200),
-                        backgroundImage:
-                            AssetImage('image/docmeds.png'), // Your image here
-                        radius:
-                            50.0, // Smaller radius to not cover much of the QR code
-                      ),
-                    ),
-                  ],
+                Positioned(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    backgroundImage:
+                        AssetImage('image/docmeds.png'), // Your image here
+                    radius:
+                        50.0, // Smaller radius to not cover much of the QR code
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'LOCATION :',
+                style: TextStyle(
+                  color: second,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                'Pharmacy',
+                style: TextStyle(
+                    color: primary, fontSize: 20, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50),
+            // color: Colors.amber,
+            width: 600,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('STEPS',
+                    style: TextStyle(
+                        color: primary,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: primary,
+                      child: Text(
+                        '1',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '>>',
+                      style: TextStyle(color: second, fontSize: 20),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Scan the QR code',
+                        style: TextStyle(
+                            color: primary,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold))
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: primary,
+                      child: Text(
+                        '2',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '>>',
+                      style: TextStyle(color: second, fontSize: 20),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Select department',
+                        style: TextStyle(
+                            color: primary,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold))
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: primary,
+                      child: Text(
+                        '3',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '>>',
+                      style: TextStyle(color: second, fontSize: 20),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Fill the form and Submit',
+                        style: TextStyle(
+                            color: primary,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold))
+                  ],
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 145,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'POWERD BY',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'DOC',
+                style: TextStyle(
+                    color: primary, fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Text(
+                'MEDS',
+                style: TextStyle(
+                    color: second, fontWeight: FontWeight.bold, fontSize: 20),
+              )
+            ],
+          )
         ],
       ),
     );
